ce813ef29375c84a9db19c95d31f55858f68b72b
sogand <s.zamanikhah@yahoo.com>
2020-03-27 22:12:34 +0430

    weather_graphic2

---
+++ app/src/main/java/com/example/volley_ex/main_page1.java
@@ -0,0 +1,14 @@
+package com.example.volley_ex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class main_page1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page1);
+    }
+}
